89aa1d452bf32a48d6392744aac0ba655528b8e2
chibuisi <enyia21@gmail.com>
2020-05-18 15:39:28 -0500

    Done.

---
+++ lib/update.sql
@@ -0,0 +1,7 @@
+UPDATE characters SET species= "Martian" WHERE species = 'cylon' AND id = 8;
+-- INSERT INTO characters (id, name, motto, species, author_id) 
+-- VALUES 
+-- (5, "Character One", "motto one", "cylon", 2), 
+-- (6, "Character Two", "motto two", "human", 2), 
+-- (7, "Character Three", "motto three", "cylon", 2), 
+-- (8, "Character Four", "motto four", "cylon", 2);
